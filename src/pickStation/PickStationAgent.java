682eb8951e21987f82a8956478aa234b5c9d3c15
Laverden <nlav05@gmail.com>
2014-11-11 16:35:04 +0100

    V.1.0.N. PickStationAgent and RobotAgent basic skellington. Comunication between PickerAgent and RobotAgents established.

--- src/pickStation/PickStationAgent.java
+++ src/pickStation/PickStationAgent.java
@@ -1,8 +1,8 @@
 /**
 COPYRIGHT NOTICE (C) 2014. All Rights Reserved.   
 Project: KivaSolutions
-@author: Argentina Ortega Sainz, Nicol‡s Laverde Alfonso & Diego Enrique Ramos Avila
-@version: 1.0 
+@author: Argentina Ortega Sainz, Nicolï¿½s Laverde Alfonso & Diego Enrique Ramos Avila
+@version: 2.0.n.
 @since 09.11.2014 
 HBRS - Multiagent Systems
 All Rights Reserved.  
@@ -10,11 +10,68 @@ All Rights Reserved.
 
 package pickStation;
 
+import jade.core.AID;
 import jade.core.Agent;
+import jade.core.behaviours.TickerBehaviour;
+import jade.domain.DFService;
+import jade.domain.FIPAException;
+import jade.domain.FIPAAgentManagement.DFAgentDescription;
+import jade.domain.FIPAAgentManagement.ServiceDescription;
+import jade.lang.acl.ACLMessage;
 
+@SuppressWarnings("serial")
 public class PickStationAgent extends Agent {
-	protected void setup(){
-		System.out.println("Hello, I am "+getLocalName());
+	
+	// Private String name;
+	private AID[] activeAgent;
+	// Setup for the PickStation Agent.
+	protected void setup() {
+		// Initialization Messages
+		System.out.println("--PICKER-------------");
+		System.out.println("Agent: " + this.getAID().getLocalName());
+		System.out.println("Picker Launched!");
+		System.out.println("--------------------------\n\n");
+		// Behavior for searching robots subscribed to the yellow pages.
+		this.addBehaviour(new TickerBehaviour(this, 30000) {
+			protected void onTick() {
+				// Update the list of robot agents.
+				DFAgentDescription template = new DFAgentDescription();
+				ServiceDescription sd = new ServiceDescription();
+				// Search for agents who offer a fetch service.
+				sd.setType("fetch");
+				// 
+				template.addServices(sd);
+				try {
+					// Searching process.
+					DFAgentDescription[] result = DFService.search(myAgent, template); 
+					System.out.println("\n\n-SEARCHING FOR AGENTS---------------");
+					System.out.println("Found the following active agents:");
+					activeAgent = new AID[result.length];
+					// Found Agents.
+					for (int i = 0; i < result.length; ++i) {
+						// Listing the agents ID's found.
+						activeAgent[i] = result[i].getName();
+						System.out.println(activeAgent[i].getName());
+					}
+					System.out.println("------------------------------------\n");
+					/* Sending Messages to the found agents. */
+					ACLMessage cfp = new ACLMessage(ACLMessage.CFP);
+					for (int i = 0; i < result.length; ++i) {
+						cfp.addReceiver(result[i].getName());
+					}
+					cfp.setContent("Where are thou?");
+					myAgent.send(cfp);
+				}
+				catch (FIPAException fe) {
+					fe.printStackTrace();
+				}
+			}
+		} );
+		
+	}
+	// Killing the agent.
+	protected void takeDown() {
+		System.out.println("Cordinator Killed!!!!!!!!.");
 	}
 
 }
--- src/robots/RobotAgent.java
+++ src/robots/RobotAgent.java
@@ -1,8 +1,8 @@
 /**
 COPYRIGHT NOTICE (C) 2014. All Rights Reserved.   
 Project: KivaSolutions
-@author: Argentina Ortega Sainz, Nicol‡s Laverde Alfonso & Diego Enrique Ramos Avila
-@version: 1.0 
+@author: Argentina Ortega Sainz, Nicolas Laverde Alfonso & Diego Enrique Ramos Avila
+@version: 2.0.n.
 @since 09.11.2014 
 HBRS - Multiagent Systems
 All Rights Reserved.  
@@ -10,12 +10,87 @@ All Rights Reserved.
 
 package robots;
 
+import java.util.Random;
 import jade.core.Agent;
+import jade.core.behaviours.CyclicBehaviour;
+import jade.core.behaviours.OneShotBehaviour;
+import jade.domain.DFService;
+import jade.domain.FIPAException;
+import jade.domain.FIPAAgentManagement.DFAgentDescription;
+import jade.domain.FIPAAgentManagement.ServiceDescription;
+import jade.lang.acl.ACLMessage;
 
-
+@SuppressWarnings("serial")
 public class RobotAgent extends Agent {
-
-	protected void setup(){
-		System.out.println("Hello, I am "+getLocalName());
+	// Attributes.
+	private String area;
+	private double[] position;
+	
+	protected void setup() {
+		System.out.println("Robot Agent: " + getLocalName());
+		
+		this.position = new double[2];
+		this.position[0] = (new Random()).nextDouble();
+		this.position[1] = (new Random()).nextDouble();
+		
+		DFAgentDescription dfd = new DFAgentDescription();
+		dfd.setName(getAID());
+		ServiceDescription sd = new ServiceDescription();
+		sd.setType("fetch");
+		sd.setName("JADE-robot-agents");
+		dfd.addServices(sd);
+		
+		Object[] args = this.getArguments();
+		if (args != null && args.length > 0) {
+			this.area = (String) args[0];
+			System.out.println("Area " + this.area + " will be covered.");
+			System.out.println("Waiting for commands...");
+			this.addBehaviour(new OfferRequestsServer());
+			try {
+				DFService.register(this, dfd);
+			}
+			catch (FIPAException fe) {
+				fe.printStackTrace();
+			}
+		}
+		else {
+			System.out.println("No arguments are given. Agent didn't spawn");
+			doDelete();
+		}
 	}
+	
+	public class LocalizationBehaviour extends OneShotBehaviour {
+		public void action() {
+			System.out.println("Location: (" + position[0] + "," + position[1] + ").\n");			
+		}
+		
+	}
+	
+	private	class OfferRequestsServer extends CyclicBehaviour {
+		public void action() {
+			ACLMessage msg = myAgent.receive();
+				if (msg != null) {
+					String mensaje = msg.getContent();
+					System.out.println(myAgent.getLocalName() + " here.");
+					System.out.println("Message: " + mensaje);
+					myAgent.addBehaviour(new LocalizationBehaviour());
+					System.out.println(myAgent.getLocalName() + " out.");
+				}
+				else {
+					block();
+				}
+		}
+	}
+	// End of inner class OfferRequests Server
+	
+	protected void takeDown() {
+		System.out.println("Agent out of service.");
+		try {
+			DFService.deregister(this);
+		}
+		catch (FIPAException fe) {
+			fe.printStackTrace();
+		}
+	}
+
 }
