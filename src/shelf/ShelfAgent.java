62cad2bd92dbc9a08a348aa165f2a4884a33e340
Laverden <nlav05@gmail.com>
2014-11-12 11:01:14 +0100

    Unbesonderheit: renaming of "bookshelf" agent for shelf agent.

---
+++ src/shelf/ShelfAgent.java
@@ -0,0 +1,20 @@
+/**
+COPYRIGHT NOTICE (C) 2014. All Rights Reserved.   
+Project: KivaSolutions
+@author: Argentina Ortega Sainz, Nicol�s Laverde Alfonso & Diego Enrique Ramos Avila
+@version: 1.0 
+@since 09.11.2014 
+HBRS - Multiagent Systems
+All Rights Reserved.  
+**/
+
+package shelf;
+
+import jade.core.Agent;
+
+public class ShelfAgent extends Agent {
+	protected void setup(){
+		System.out.println("Hello, I am "+getLocalName());
+	}
+
+}
