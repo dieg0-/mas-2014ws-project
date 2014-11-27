5c27ab5996230fdefa84daf889e53525fa137b45
Laverden <nlav05@gmail.com>
2014-11-27 18:03:25 +0100

    V.3.5.N.00.B. Documention and chingo-ful of comment. Better structuration of the print outs. Added TODO's to the classes to track what is left to do.

--- src/station/RobotAgent.java
+++ src/station/RobotAgent.java
@@ -2,7 +2,7 @@
 COPYRIGHT NOTICE (C) 2014. All Rights Reserved.   
 Project: KivaSolutions
 @author: Argentina Ortega Sainz, Nicolas Laverde Alfonso & Diego Enrique Ramos Avila
-@version: 3.3.n.
+@version: 3.5.n.
 @since 27.11.2014 
 HBRS - Multiagent Systems
 All Rights Reserved.  
@@ -21,6 +21,18 @@ import jade.domain.FIPAAgentManagement.Property;
 import jade.lang.acl.ACLMessage;
 import utilities.Pose;
 
+/**
+ * Robot agent offers a service of "fetch" type when the agent is free. The
+ * task of this agent is to fetch a shelf commanded by the {@link PickerAgent}.
+ * The do not communicate directly with the shelves; instead, the communication
+ * is done indirectly through the {@link PickerAgent}.
+ * <p>
+ * Attributes:
+ * <li> id - the unique identification number of the robot.
+ * <li> position - an instance of the class {@link Pose} with the robot position.
+ * <li> busy - flag to reflect the status of the robot.
+ * <li> dfd - agent description with the services offered.
+ */
 @SuppressWarnings("serial")
 public class RobotAgent extends Agent {
 	// Attributes.
@@ -30,16 +42,21 @@ public class RobotAgent extends Agent {
 	protected DFAgentDescription dfd;
 	
 	protected void setup() {
+		// PRINTOUTS: Initialization Messages
 		System.out.println("\n--ROBOT------------------");
 		System.out.println("Agent: " + getLocalName());
-		
+		// Random initialization of the position of the robot.
 		this.position = new Pose();
 		this.position.randomInit(true);
 		this.busy = false;
-		
+		// Agent Description.
 		this.dfd = new DFAgentDescription();
 		this.dfd.setName(getAID());
-		
+		/* Service offer -> fetch. It serves as the criteria to find robots
+		 * which are free (not busy) to delegate the task of fetching a shelf.
+		 * The service includes a property called position, which contains the
+		 * current position of the robot as a string.
+		 */
 		ServiceDescription sd = new ServiceDescription();
 		sd.setType("fetch");
 		sd.setName("Fetch-Service");
@@ -48,13 +65,21 @@ public class RobotAgent extends Agent {
 		p.setValue(position.parsePose());
 		sd.addProperties(p);
 		this.dfd.addServices(sd);
-		
+		/* The robot agent must be provided with an argument which contains
+		 * the identification number of the agent. If such argument is not
+		 * provided, the agent won't be created.
+		 */
 		Object[] args = this.getArguments();
 		if (args != null && args.length > 0) {
 			this.id = (String) args[0];
+			// PRINTOUTS: ID Message.
 			System.out.println("ID: " + this.id);
 			System.out.println("  > Waiting.");
+			// Waiting for the picker agent to request the robot to fetch.
 			this.addBehaviour(new OfferRequestsServer());
+			/* Trying to register the Agent Description into the description
+			 * facilitator (DF).
+			 */
 			try {
 				DFService.register(this, dfd);
 			}
@@ -69,37 +94,70 @@ public class RobotAgent extends Agent {
 		System.out.println("-------------------------\n");
 	}
 	
-	
+	/**
+	 * Behavior which is executed one time if the message sent by the picker
+	 * has the ontology "localization". It will returns the current position
+	 * of the robot.
+	 * @return position of the robot agent.
+	 */
 	private class LocalizationBehaviour extends SimpleBehaviour {
-		
+		/**
+		 * Override constructor of the SimpleBehavior.
+		 * @param a	this agent.
+		 */
 		public LocalizationBehaviour(Agent a) {
 			super(a);			
 		}
-		
+		/**
+		 * Main action of the behavior. It returns the position of the
+		 * robot as a print out.
+		 */
 		public void action() {
 			String current_pos = String.format("(%.2f, %.2f)", position.getX(), position.getY());
 			System.out.println("  > Location: " + current_pos + ").");
 			System.out.println("--------------------\n");
 			block(250);
 		}
-		
+	
 		public boolean done() {
 			return true;
 		}
 	}
 	
-	
+	/**
+	 * Behavior which simulates the action of fetch a given shelf. It puts
+	 * the thread of the agent into sleep for a given amount of time. The
+	 * time should simulate the distance the robot must travel in order to
+	 * reach the shelf and brought it back to the station.
+	 * <p>
+	 * Attributes:
+	 * <li> timeout - duration which the thread will be sleeping.
+	 * <li> target - position of the shelf as a string.
+	 */
 	private class FetchBehaviour extends SimpleBehaviour {
-		//Simulated the fetching time, in seconds.
+		
 		private long timeout;
 		private String target;
-		
+		/**
+		 * Override constructor of the SimpleBehavior.
+		 * @param a					this agent.
+		 * @param time_sleep		duration of sleeping.
+		 * @param shelf_position	position of the shelf.
+		 */
 		public FetchBehaviour(Agent a, long time_sleep, String shelf_position) {
 			super(a);
 			this.timeout = time_sleep;
 			this.target = shelf_position;
 		}
-		
+		/**
+		 * Main action of the behavior. It works as follow:
+		 * <li> The robot is de-register from the descriptor facilitator DF to
+		 * inform it is not available anymore. It means that the picker
+		 * agent won't find this agent when it search for free robots.
+		 * <li> Thread is putting to sleep by the amount of time specified.
+		 * <li> As soon as it wakes ups, announcing that it has returned the
+		 * shelf to its original position, the done() method is executed.
+		 */
 		public void action() {
 			System.out.println(myAgent.getLocalName() + ": [fetching].");
 			System.out.println("  > Target at: " + this.target);
@@ -115,24 +173,18 @@ public class RobotAgent extends Agent {
 			catch (Exception e) {
 				System.err.println(myAgent.getLocalName() + ": terminated abruptly.");
 			}
-			//System.out.println(myAgent.getState());	
 		}
-		
+		/**
+		 * Successfully completing the fetch action of a specified shelf. The
+		 * agent tries to re-register into the description facilitator DF to
+		 * announce it is free again, and therefore, it can be found again by
+		 * the picker agent.
+		 */
 		public boolean done() {
 			System.out.println("\n------------------------------------");
 			System.out.println(myAgent.getLocalName() + ": [report].");
 			System.out.println("  > Task accomplished.");
 			System.out.println("------------------------------------\n");
-			/*
-			ServiceDescription sd = new ServiceDescription();
-			sd.setType("fetch");
-			sd.setName("Fetch-Service");
-			Property p = new Property();
-			p.setName("position");
-			p.setValue(position.parsePose());
-			sd.addProperties(p);
-			dfd.addServices(sd);
-			*/
 			try {
 				DFService.register(myAgent, dfd);
 			}
@@ -143,17 +195,34 @@ public class RobotAgent extends Agent {
 		}
 	}
 	
-	
+	/**
+	 * Cyclic behavior which is constantly executed. It waits until
+	 * a message form the picker agent is sent to this agent to
+	 * execute the action() method.
+	 */
 	private	class OfferRequestsServer extends CyclicBehaviour {
+		/**
+		 * Main action of the behavior. When a message is received,
+		 * this agent will inspect the ontology of the message and
+		 * will execute the given command. Until now, three types
+		 * of commands can be given to the robot:
+		 * <li> status: the agent informs the picker its status. [Deprecated]
+		 * <li> localization: the agent informs the picker its location.
+		 * <li> fetch: the agent executes the FetchBehavior.
+		 * When other ontology is sent to this agent, it will do nothing and 
+		 * will inform that the commanded action is not recognized or implemented.
+		 */
 		public void action() {
 			ACLMessage msg = myAgent.receive();
 			if (msg != null) {
 				System.out.println("--------------------------");
 				String msg_content = msg.getContent();
 				String msg_command = msg.getOntology();
+				// PRINTOUTS: Message received with the commanded action.
 				System.out.print(myAgent.getLocalName() + ": ");
 				System.out.println("[message received].");
 				System.out.println("  > Command: " + msg_command);
+				// Inspection of the commanded action.
 				if (msg_command.matches("status")) {
 					System.out.println("  > Busy: " + busy);
 				}
@@ -171,9 +240,10 @@ public class RobotAgent extends Agent {
 			block();
 		}
 	}
-	// End of inner class OfferRequests Server
 	
-	
+	/**
+	 * Safe delete of the agent.
+	 */
 	protected void takeDown() {
 		System.out.println(this.getLocalName() + " out of service.");
 		try {
